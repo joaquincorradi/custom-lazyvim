c749d0b424de6f43f5f4a7738450b56de4406099
Joaquin I. Corradi (macOS) <corradijoaquin@gmail.com>
2023-08-07 18:48:51 -0300

    First commit

---
+++ lua/plugins/dracula.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "Mofiqul/dracula.nvim",
+    opts = {
+      transparent_bg = true,
+      italic_comment = true,
+    },
+  },
+}
---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "python",
+        "tsx",
+        "typescript",
+        "rust",
+        "json",
+        "bash",
+        "lua",
+        "vim",
+        "gitignore",
+        "cpp",
+        "c",
+        "cmake",
+        "fish",
+        "toml",
+        "html",
+        "css",
+        "javascript",
+        "latex",
+      },
+    },
+  },
+}
